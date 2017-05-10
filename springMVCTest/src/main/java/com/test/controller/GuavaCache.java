018a7d62685c6a458ff143e7d7239367eb493d30
schemeGOME <2974576553@qq.com>
2017-05-10 19:47:02 +0800

    项目调试已可启动

---
+++ springMVCTest/src/main/java/com/test/controller/GuavaCache.java
@@ -0,0 +1,46 @@
+package com.test.controller;
+
+import com.google.common.base.Optional;
+import com.google.common.cache.*;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @ClassName: GuavaCache
+ * @Description: (Description)
+ * @Author: songzhengjie
+ * @DATE: 2017/5/10 17:18
+ */
+public class GuavaCache {
+
+
+    public static void main(String[] args) {
+
+        LoadingCache<String, Optional<String>> loadingCache = CacheBuilder
+                .newBuilder()
+                .expireAfterWrite(3, TimeUnit.SECONDS)
+                .removalListener(new RemovalListener<String, Optional<String>>() {
+                    @Override
+                    public void onRemoval(RemovalNotification<String, Optional<String>> removalNotification) {
+                        System.out.println("cache expired ,delete key:" + removalNotification.getKey());
+                    }
+                })
+                .build(new CacheLoader<String, Optional<String>>() {
+                    @Override
+                    public Optional<String> load(String key) throws Exception {
+                        return Optional.fromNullable("取到默认值default!!!");
+                    }
+                });
+
+        try {
+            System.out.println("load form cache once :" +loadingCache.get("github").orNull());
+        } catch (ExecutionException e) {
+
+
+        }
+    }
+
+}
--- springMVCTest/src/main/java/com/test/controller/main.java
+++ springMVCTest/src/main/java/com/test/controller/main.java
@@ -1,11 +1,17 @@
 package com.test.controller;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.graph.Graphs;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.security.Key;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @ClassName: main
@@ -22,6 +28,15 @@ public class main {
     @RequestMapping("/home")
     public String home(HttpServletRequest request, HttpServletResponse response){
         ModelAndView mv = new ModelAndView();
+
+        LoadingCache<Key, Graphs> graphs = CacheBuilder.newBuilder().maximumSize(1000).expireAfterWrite(10, TimeUnit.MINUTES).removalListener(null).build(
+                new CacheLoader<Key, Graphs>() {
+                    @Override
+                    public Graphs load(Key key) throws Exception {
+                        return null;
+                    }
+                }
+        );
 
         return "index";
     }
